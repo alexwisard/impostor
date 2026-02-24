50b727fb975aee5a150521a50bf922fc9ced2565
Oleksandr Petrusha <alexander.petrusha@gmail.com>
2026-02-24 18:38:32 +0200

    added a type declaration for the Tally object on the Window interface to avoid TypeScript errors when calling window.Tally.loadEmbeds() in the RegistrationSection component.

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,12 @@
+declare global {
+  interface Window {
+    Tally?: {
+      loadEmbeds?: () => void;
+    };
+  }
+}
+export {};
+
 export { Home } from "./Home";
 export { NotFound } from "./NotFound";
 export { ThankYou } from "./ThankYou";
